ec4399a15197f50cb469da843f41f315d485b8db
yubinex <janhauck99@gmail.com>
2025-08-06 01:18:34 +0200

    chore: Clean up initial project setup
    Removed boilerplate code from `lib/identicon.ex` and `test/identicon_test.exs` to prepare for new feature development.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,2 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
