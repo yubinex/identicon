6d07cccf5a2e1d08bd2e8e4c2d0532cad60e7a64
yubinex <janhauck99@gmail.com>
2025-08-06 01:58:01 +0200

    feat: Add binary input validation to main and hash_input functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,10 +1,10 @@
 defmodule Identicon do
-  def main(input) do
+  def main(input) when is_binary(input) do
     input
     |> hash_input()
   end
 
-  def hash_input(input) do
+  def hash_input(input) when is_binary(input) do
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list()
   end
