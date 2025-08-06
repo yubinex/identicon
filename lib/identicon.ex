6e23af54a7a97a9850604d9e4c9513580d039314
yubinex <janhauck99@gmail.com>
2025-08-06 02:22:39 +0200

    feat: Implement pick_color function to extract RGB values from image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,13 @@ defmodule Identicon do
   def main(input) when is_binary(input) do
     input
     |> hash_input()
+    |> pick_color()
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    [r, g, b]
   end
 
   def hash_input(input) when is_binary(input) do
